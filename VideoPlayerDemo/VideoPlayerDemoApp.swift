f1ef788b4c7ebaa95b80a142f67f6e2ac65f512a
ming <k20057@yahoo.com.tw>
2022-11-28 16:23:54 +0800

    Initial Commit

---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by ming on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
