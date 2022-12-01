59186ae7dfa5b0c941a2dd57e8c5757725ca97c6
k20057 <k20057@yahoo.com.tw>
2022-12-01 10:06:29 +0800

    finish

---
+++ VideoPlayerDemo/Views/VideoView.swift
@@ -0,0 +1,32 @@
+//
+//  VideoView.swift
+//  VideoPlayerDemo
+//
+//  Created by ming on 2022/11/30.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoView: View {
+    var video: Video
+    @State private var player = AVPlayer()
+    
+    var body: some View {
+        VideoPlayer(player: player)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                if let link = video.videoFiles.first?.link {
+                    player = AVPlayer(url: URL(string: link)!)
+                    
+                    player.play()
+                }
+            }
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView(video: previewVideo)
+    }
+}
